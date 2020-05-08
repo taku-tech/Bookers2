b2156d82d654446ef298112de0670f2b9eb86846
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-08 15:58:36 +0900

    [Add]nameデータの操作許可設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	# nameのデータ操作を可能にする設定
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	
+	protected
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:sign_up, key: [:name])
+		end
 end
