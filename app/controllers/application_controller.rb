69234cf808f0bf8370ffcd384df98d9e97919e49
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-08 16:22:25 +0900

    [Fix]key->keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
 	# nameのデータ操作を可能にする設定
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	
+
 	protected
 		def configure_permitted_parameters
-			devise_parameter_sanitizer.permit(:sign_up, key: [:name])
+			devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
 		end
 end
