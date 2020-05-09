d370a8aa7f076dfa83f6a921609e3a40130aa1b4
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-09 12:52:42 +0900

    [Fix]エラー修正「,」抜け

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'homes/about' => 'homes#about'
   devise_for :users
-  resources :books only:[:index, :create, :show]
+  resources :books, only:[:index, :create, :show]
 end
