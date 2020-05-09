22a83f31c15135915fa42a92af0e8f344e22b97d
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-09 12:53:26 +0900

    [Update]サインイン、ログイン後のページ遷移先

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@ class ApplicationController < ActionController::Base
 	# nameのデータ操作を可能にする設定
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	def after_sign_in_path_for(resorce)
+		books_path
+	end
+
+	def after_sign_up_path_for(resource)
+		books_path
+	end
+
+
 	protected
 		def configure_permitted_parameters
 			devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
