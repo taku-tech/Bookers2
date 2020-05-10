74e8b099b3b2b51b0dac30db6393acceec4a9f65
北間匠 <takumi_mac@kitakantakuminoMacBook-Pro.local>
2020-05-10 14:11:28 +0900

    [Add]プロフィール編集機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '476cc287d7d9f8c4dd2dd9b027d14053748f7fc20b817b878203be3f9b7f66f4cf16404ed389c901832d32939e2f378f7d2832d8814967cf933740ead74a68f5'
